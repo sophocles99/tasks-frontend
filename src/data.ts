abf6eef1f0075d7db94a2afbea64406ca03f705d
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2025-03-29 17:34:40 +0000

    remove console.log statements

--- src/data.ts
+++ src/data.ts
@@ -2,29 +2,19 @@ import { Category, dummyCategories } from './categories';
 import { TaskStatus, dummyTasks } from './tasks';
 
 export const getTasks = () => {
-  console.log(`getTasks(): ${JSON.stringify(dummyTasks)}`);
   return dummyTasks;
 };
 
 export const getCateogries = () => {
-  const dummyCategoriesWithTaskData: Category[] = dummyCategories.map(
-    (category) => {
-      const tasksInCategory = dummyTasks.filter((task) =>
-        task.categoryIds.includes(category.id),
-      );
-      const tasksDoneInCategory = tasksInCategory.filter(
-        (task) => task.status == TaskStatus.Done,
-      );
-      return {
-        ...category,
-        taskCount: tasksInCategory.length,
-        taskDoneCount: tasksDoneInCategory.length,
-        taskIds: tasksInCategory.map((task) => task.id),
-      };
-    },
-  );
-  console.log(
-    `getCategories(): ${JSON.stringify(dummyCategoriesWithTaskData)}`,
-  );
+  const dummyCategoriesWithTaskData: Category[] = dummyCategories.map((category) => {
+    const tasksInCategory = dummyTasks.filter((task) => task.categoryIds.includes(category.id));
+    const tasksDoneInCategory = tasksInCategory.filter((task) => task.status == TaskStatus.Done);
+    return {
+      ...category,
+      taskCount: tasksInCategory.length,
+      taskDoneCount: tasksDoneInCategory.length,
+      taskIds: tasksInCategory.map((task) => task.id),
+    };
+  });
   return dummyCategoriesWithTaskData;
 };
